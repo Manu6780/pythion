1251eeb19eefcf9314553d10a841ed06dd9ddadc
manu240586 <wesofttech3@gmail.com>
2023-12-25 04:02:25 +0530

    converter

---
+++ Streamlit functions.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("What to do")
+st.subheader("whatever you want to")
+st.write("kyun bakchodi kar raha hai")
+st.checkbox("kyun bhai",key="check")
+st.text_input("",key="test")
+st.session_state
--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 import PySimpleGUI as sg
 import functions
 
+sg.theme("Black")
+
 label1 = sg.Text("Enter feet: ")
 input_text1 = sg.Input(key="feet")
 label2 = sg.Text("Enter inches: ")
 input_text2 = sg.Input(key="inches")
 button = sg.Button("Convert")
 label3 = sg.Text("", key="result")
-
+button2 = sg.Button("Exit")
 window = sg.Window("Convertor",
                    layout=[[label1, input_text1],
                            [label2, input_text2],
-                           [button, label3]])
+                           [button, button2, label3]])
 while True:
-   event, values = window.read()
-   value_feet = float(values['feet'])
-   value_inches = float(values['inches'])
+    event, values = window.read()
+    value_feet = float(values['feet'])
+    value_inches = float(values['inches'])
 
-   value_meter = functions.meter_converter(value_feet,value_inches)
-   window['result'].update(value=f"{value_meter} m", text_color="red")
+    value_meter = functions.meter_converter(value_feet, value_inches)
+    window['result'].update(value=f"{value_meter} m", text_color="red")
 
+    match event:
+        case "Exit":
+            break
+        case sg.WIN_CLOSED:
+            break
 
+print("done")
 window.close()
